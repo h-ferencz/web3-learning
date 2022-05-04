70662cd27e728f8a8705f70d1726b627f9d7da31
huba <example@dot.com>
2022-05-04 16:15:29 +0300

    final

--- dice-site/index.js
+++ dice-site/index.js
@@ -1,9 +1,21 @@
 function rollDice() {
-  var playerOne = Math.floor(Math.random() * 6) + 1 + ".png";
-  var playerTwo = Math.floor(Math.random() * 6) + 1 + ".png";
+  var playerOne = Math.floor(Math.random() * 6) + 1;
+  var playerTwo = Math.floor(Math.random() * 6) + 1;
 
-  document.querySelector(".player1").setAttribute("src", playerOne);
-  document.querySelector(".player2").setAttribute("src", playerTwo);
+  if (playerOne > playerTwo) {
+    document.querySelector(".player2cup").classList.add("hide");
+    document.querySelector(".player1cup").classList.remove("hide");
+  } else if (playerOne < playerTwo) {
+    document.querySelector(".player1cup").classList.add("hide");
+    document.querySelector(".player2cup").classList.remove("hide");
+  } else if (playerOne == playerTwo) {
+    document.querySelector(".player1cup").classList.remove("hide");
+    document.querySelector(".player2cup").classList.remove("hide");
+  }
+
+
+  document.querySelector(".player1").setAttribute("src", playerOne + ".png");
+  document.querySelector(".player2").setAttribute("src", playerTwo + ".png");
 
 
 }
