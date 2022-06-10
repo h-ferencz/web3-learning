e02e98140eefd8ae58308cd0eb60c83e60324e89
black-rock-ceo <102721553+black-rock-ceo@users.noreply.github.com>
2022-06-10 20:16:23 +0300

    Update index.js

--- Simon Game/index.js
+++ Simon Game/index.js
@@ -1,31 +1,98 @@
-
-
-
+var computerColorList = [];
+var userColorList = [];
 
 // random color
-
 function randomColor() {
     var colors = ["green", "red", "yellow", "blue"];
     var randomColor = colors[Math.floor(Math.random() * colors.length)];
+
     return randomColor
 }
 
 
-$(document).on('keypress',function() {
+
+// START THE GAME
+$(document).on('keypress', function (e) {
+
+    computerColorList = [];
+    userColorList = [];
+
+
+
     var c = randomColor();
-    $("."+c).addClass("pressed");
-    setTimeout(function() { 
-        $("."+c).removeClass("pressed");
+    computerColorList.push(c)
+    // choosing the right sound for the block
+    var sound = new Audio("sounds/" + c + ".mp3");
+    $("." + c).addClass("pressed");
+    sound.play();
+    setTimeout(function () {
+        $("." + c).removeClass("pressed");
     }, 100);
+    checkResult();
+    $("#level-title").text("Press A Key to Start");
+
 });
 
 
-// // click effect
+// DETECT USER CHOICE
+$(".btn").on("click", function (event) {
 
-$(".btn").on("click", function(event) {
-    $("."+event.target.id).addClass("pressed");
-    setTimeout(function() { 
-        $("."+event.target.id).removeClass("pressed");
+
+    c = event.target.id
+    var sound = new Audio("sounds/" + c + ".mp3");
+    // pushing the color to user list
+    userColorList.push(c)
+    $("." + c).addClass("pressed");
+    sound.play()
+    setTimeout(function () {
+        $("." + c).removeClass("pressed");
     }, 100);
+    checkResult();
 })
 
+
+// CHECK USER'S CHOICE
+function checkResult() {
+
+    console.log("user: " + userColorList);
+    console.log("computer: " + computerColorList);
+
+    if (userColorList.length <= 0) {
+        $("#level-title").text("Follow the pattern!");
+    } else if (JSON.stringify(computerColorList.slice(0, userColorList.length)) === JSON.stringify(userColorList) && computerColorList.length != userColorList.length) {
+        $("#level-title").text("Go on! Score: " + computerColorList.length);
+    } else if (JSON.stringify(computerColorList) === JSON.stringify(userColorList)) {
+
+        
+
+        setTimeout(function () {
+            var c = randomColor();
+            computerColorList.push(c)
+            // choosing the right sound for the block
+            var sound = new Audio("sounds/" + c + ".mp3");
+            $("." + c).addClass("pressed");
+            sound.play()
+            setTimeout(function () {
+                $("." + c).removeClass("pressed");
+            }, 100);
+            checkResult();
+            userColorList = [];
+        }, 1000);
+
+        $("#level-title").text("Go on! Score: " + computerColorList.length);
+        
+    } else {
+        var sound = new Audio("sounds/wrong.mp3");
+        sound.play();
+
+        $("body").css("background-color","red");
+        setTimeout(function () {
+            $("body").css("background-color","#195ca3");
+        }, 100);
+        $("#level-title").text("Press any key to restart. Final score: " + computerColorList.length);
+
+        computerColorList = [];
+        userColorList = [];
+        
+    }
+}
