1e0e21c9a4af5420c71614d6fb73a92ec53b6fa9
black-rock-ceo <102721553+black-rock-ceo@users.noreply.github.com>
2022-06-11 15:40:11 +0300

    Create index.js

---
+++ nodejs-learn/index.js
@@ -0,0 +1,10 @@
+var superheroes = require("superheroes");
+
+var mySuperheroName = superheroes.random();
+
+console.log(mySuperheroName);
+
+const supervillains = require('supervillains');
+ 
+
+console.log(supervillains.random());
