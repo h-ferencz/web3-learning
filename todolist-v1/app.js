137c03a3be5c9f1373f49be65139cc10b143fd9c
Huba Ferencz <hubaferencz@Hubas-MacBook-Air.local>
2022-06-16 11:56:59 +0300

    finishing todo-app

--- todolist-v1/app.js
+++ todolist-v1/app.js
@@ -1,44 +1,40 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const date = require(__dirname + "/date.js");
 
 const app = express();
 
+let newItems = [];
+let workItems = [];
+
 app.set('view engine', 'ejs');
 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
 app.get("/", function (req, res) {
-
-    var today = new Date();
-    var todaysDate = today.getDay();
-    var day = "";
-
-    switch (todaysDate) {
-        case 0:
-            day = "Sunday";
-            break;
-        case 1:
-            day = "Monday";
-            break;
-        case 2:
-            day = "Tuesday";
-            break;
-        case 3:
-            day = "Wednesday";
-            break;
-        case 4:
-            day = "Thursday";
-            break;
-        case 5:
-            day = "Friday";
-            break;
-        case 6:
-            day = "Saturday";
-            break;
-
-        default:
-            console.log("Error: current day is equal to " + todaysDate);
-    }
-    res.render("list", { kindOfDay: day });
+    let todaysDate = date.getDate();
+    res.render("list", { listTitle: todaysDate, newItems: newItems });
 })
+
+
+app.post("/", function (req, res) {
+    
+    let item = req.body.newItem;
+
+    if (req.body.list === "Work") {
+        workItems.push(item);
+        res.redirect("/work");
+    } else {
+        newItems.push(item)
+        res.redirect("/");
+    }
+
+})
+
+app.get("/work", function (req, res) {
+    res.render("list", { listTitle: "Work TO-DO", newItems: workItems });
+});
 
 app.listen(3000, function () {
     console.log("todo-list v1 is up and running on port 3000")
