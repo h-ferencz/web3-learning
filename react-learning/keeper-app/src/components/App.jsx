1b590d95de2e189c946e43cd24fed05eaf9517f5
Huba Ferencz <hubaferencz@Hubas-MacBook-Air.local>
2022-07-29 19:38:32 +0300

    qwerty1

--- react-learning/keeper-app/src/components/App.jsx
+++ react-learning/keeper-app/src/components/App.jsx
@@ -2,15 +2,25 @@ import React from "react";
 import Heading from "./Heading";
 import Note from "./Note";
 import Footer from "./Footer";
+import note from "../notes.js"
 
 function App() {
   return (
     <div>
       <Heading />
-      <Note />
+      {
+        note.map(noteItem => (
+          <Note
+            key={noteItem.key}
+            title={noteItem.title}
+            content={noteItem.content}
+          />
+        ))
+      }
       <Footer />
     </div>
   );
 }
+console.log(note)
 
 export default App;
--- react-learning/keeper-app/src/components/Note.jsx
+++ react-learning/keeper-app/src/components/Note.jsx
@@ -1,17 +1,14 @@
 import React from "react";
 
-function Note() {
-  let note = {
-    title: "This is the note title",
-    content: "This is the note content"
-  }
+function Note(props) {
+  
   return (
     <div class="note">
       <h1>
-        {note.title}
+        {props.title}
       </h1>
       <p>
-        {note.content}
+        {props.content}
       </p>
     </div>
   );
